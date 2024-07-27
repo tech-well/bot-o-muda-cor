dc289ae0bb5eaca36e415c16cfdccf31b2ab64e0
wellington <wellingtoncarlos.tech@gmail.com>
2024-07-27 00:47:58 -0300

    treino-js

---
+++ script.js
@@ -0,0 +1,50 @@
+
+function roxo() {
+
+    document.querySelector('.teste').style.backgroundColor = 'purple'
+    document.querySelector('.teste').innerHTML = 'roxo'
+    document.querySelector('.teste').style.fontSize = '100px'
+    document.querySelector('.teste').style.color = 'white'
+    
+
+}
+function verde() {
+
+    document.querySelector('.teste').style.backgroundColor = 'green'
+    document.querySelector('.teste').innerHTML = 'verde'
+    document.querySelector('.teste').style.fontSize = '100px'
+    document.querySelector('.teste').style.color = 'white'
+
+}
+function amarelo() {
+
+    document.querySelector('.teste').style.backgroundColor = 'yellow'
+    document.querySelector('.teste').innerHTML = 'amarelo'
+    document.querySelector('.teste').style.fontSize = '100px'
+    document.querySelector('.teste').style.color = 'white'
+
+}
+function azul() {
+
+    document.querySelector('.teste').style.backgroundColor = 'blue'
+    document.querySelector('.teste').innerHTML = 'azul'
+    document.querySelector('.teste').style.fontSize = '100px'
+    document.querySelector('.teste').style.color = 'white'
+
+}
+function preto() {
+
+    document.querySelector('.teste').style.backgroundColor = 'black'
+    document.querySelector('.teste').innerHTML = 'preto'
+    document.querySelector('.teste').style.fontSize = '100px'
+    document.querySelector('.teste').style.color = 'white'
+
+}
+function branco() {
+
+    document.querySelector('.teste').style.backgroundColor = 'white'
+    document.querySelector('.teste').innerHTML = 'branco'
+    document.querySelector('.teste').style.fontSize = '100px'
+    document.querySelector('.teste').style.color = 'green'
+
+}
